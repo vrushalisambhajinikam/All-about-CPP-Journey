000d3db0c46563541d3bd74c16d558df28e1fe63
Vrushali Sambhaji  Nikam <110546379+vrushalisambhajinikam@users.noreply.github.com>
2024-02-23 20:53:08 -0800

    Add files via upload

---
+++ Operator Overloading unary minus(++) Operator.cpp
@@ -0,0 +1,45 @@
+/*
+
+Operator Overloading: unary minus(++) Operator
+ 
+*/
+#include<iostream>
+using namespace std;
+class Space
+{
+    private:
+    	int x,y,z;
+    public:
+    	void getdata(int m,int n,int p)
+    	{
+    		x=m;
+    		y=n;
+    		z=p;
+		}
+		void display()
+		{
+			cout<<"\nvalue of x="<<x;
+			cout<<"\nvalue of y="<<y;
+		    cout<<"\nvalue of z="<<z;
+		}
+		void operator ++();
+};
+void Space::operator ++()
+{
+	x++;
+	y++;
+	z++;
+}
+int main()
+{
+	
+	 Space s1;
+	 s1.getdata(10,20,30);
+	 cout<<"\nBefore calling operator overloading function";
+	 s1.display();
+	 ++s1;
+	 cout<<"\nAfter calling operator overloading function";
+	 s1.display();
+}
+
+
