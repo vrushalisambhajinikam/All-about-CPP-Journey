1f54580c8f925b12786887e4caeef9a3525fcb84
Vrushali Sambhaji  Nikam <110546379+vrushalisambhajinikam@users.noreply.github.com>
2024-03-22 19:01:34 +0530

    Add files via upload

---
+++ BioData.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"**********Hello here is my Bio-Data***********";
+	cout<<"\n Name              	: Vrushali";
+	cout<<"\n Age               	: 19";
+	cout<<"\n Marks             	: 88";
+	cout<<"\n Fav Color        	    : Yellow";
+	cout<<"\n Best Friend       	: Kali/Shivani";
+	cout<<"\n Got Scholarship       : Vidyadhan";
+}
